a4b650e4e8da444ea3f78cc4ca5ac3bc4049409e
Naz <naz@naz.com>
2012-11-17 21:19:29 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AdemoApp::Application.config.secret_token = 'b4cc2d3de8b5063fd0d720573515a98bed1245d5c40a55ed585790d48c03319221eeae011c8737a221af077a09272bd5e0a75101d7f6d9d589843cf6b35824fe'
